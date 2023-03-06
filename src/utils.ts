ec8454055499d3cd61723ee3c9589bc53a24f926
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2023-03-06 15:36:37 -0600

    Clone and adapt (broken tests, but works)

--- src/utils.ts
+++ src/utils.ts
@@ -2,9 +2,15 @@ export const getReplacementText = (
   line: string,
   indentation: number = 0
 ): string =>
+  //prettier-ignore
   line
     .replace(".todo", "")
     .replace(
       ")",
-      `, () => {\n${" ".repeat(indentation * 2)}\n${" ".repeat(indentation)}})`
+      `, () => {
+${" ".repeat(indentation * 2)}// Arrange
+${" ".repeat(indentation * 2)}// Act
+${" ".repeat(indentation * 2)}// Await?
+${" ".repeat(indentation * 2)}// Assert
+${" ".repeat(indentation)}})`
     );
